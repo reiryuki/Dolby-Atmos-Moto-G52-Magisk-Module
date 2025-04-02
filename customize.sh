a0e55029c7c882fb9d99bdf5cf9e9f07de1472ec
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2025-04-03 04:54:57 +0700

    v4.9

--- customize.sh
+++ customize.sh
@@ -903,7 +903,12 @@ for FILE in $FILES; do
     if [ -f $DES ]; then
       ui_print "- Detected"
       ui_print "$DES"
-      rm -f $MODPATH/system/vendor$FILE
+      NAME=`basename $FILE`
+      if echo $FILE | grep -q lib64; then
+        rm -f $MODPATH/system/vendor/lib64/$NAME
+      else
+        rm -f $MODPATH/system/vendor/lib/$NAME
+      fi
       ui_print " "
     fi
   done
@@ -916,7 +921,12 @@ for FILE in $FILES; do
     if [ -f $DES ]; then
       ui_print "- Detected"
       ui_print "$DES"
-      rm -f $MODPATH/system/vendor$FILE
+      NAME=`basename $FILE`
+      if echo $FILE | grep -q lib64; then
+        rm -f $MODPATH/system/vendor/lib64/$NAME
+      else
+        rm -f $MODPATH/system/vendor/lib/$NAME
+      fi
       ui_print " "
     fi
   done
